f87ee163042a2d50775f19d303a37774f788c091
dukz <dkz359@163.com>
2020-12-02 01:26:40 +0800

    feat: 添加新文件，测试Integer缓存

--- src/main/java/com/dukz/base/TestIntegerCache.java
+++ src/main/java/com/dukz/base/TestIntegerCache.java
@@ -1,16 +1,16 @@
-package com.dukz.test;
-
-import java.util.HashMap;
+package com.dukz.base;
 
 /**
- * @ClassName TestTemp
- * @Description TODO
+ * @ClassName TestIntegerCache
+ * @Description TestIntegerCache
+ * Integer的缓存只要使用了valueOf方法就会使用缓存，Integer的自动装箱会调用valueOf()
  * @Author dukz
- * @Date 2020-06-23
+ * @Date 2020-12-02
  * @Version 1.0
  */
-public class TestTemp {
+public class TestIntegerCache {
     public static void main(String[] args) {
+
         Integer i1 = new Integer(123);
         Integer i2 = new Integer(123);
         System.out.println(i1 == i2);
@@ -22,5 +22,7 @@ public class TestTemp {
         Integer i5 = Integer.valueOf(123);
         Integer i6 = Integer.valueOf(123);
         System.out.println(i5 == i6);
+
+        System.out.println(i3 == i5);
     }
 }
