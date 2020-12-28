bc627a3121d65949e725f76275ac92287f9db089
dukz <dkz359@163.com>
2020-12-29 01:36:39 +0800

    feat: test Jvm

---
+++ src/main/java/com/dukz/jvm/CustomClassLoader.java
@@ -0,0 +1,11 @@
+package com.dukz.jvm;
+
+/**
+ * @ClassName CustomClassLoader
+ * @Description 自定义类加载器
+ * @Author dukz
+ * @Date 12/29/20
+ * @Version 1.0
+ */
+public class CustomClassLoader {
+}
---
+++ src/main/java/com/dukz/jvm/TestWhoLoad.java
@@ -0,0 +1,22 @@
+/**
+ * @ClassName TestWhoLoad
+ * @Description 测试哪个类加载器加载
+ * @Author dukz
+ * @Date 12/29/20
+ * @Version 1.0
+ */
+public class TestWhoLoad {
+    public static void main(String[] args) {
+        // 正在使用的类加载器：sun.misc.Launcher$AppClassLoader@18b4aac2
+        ClassLoader c = TestWhoLoad.class.getClassLoader();
+        System.out.println(c);
+
+        // AppClassLoader的父加载器是：sun.misc.Launcher$ExtClassLoader@4d7e1886
+        ClassLoader c1 = c.getParent();
+        System.out.println(c1);
+
+        // ExtClassLoader的父加载器是BooStrap。它是使用C++实现的，不在JVM体系中，返回null
+        ClassLoader c2 = c1.getParent();
+        System.out.println(c2);
+    }
+}
