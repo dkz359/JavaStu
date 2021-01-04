99dd8cb8cea890dc4dd630ef73d0320eda77a6d3
dukz <dkz359@163.com>
2021-01-04 23:43:29 +0800

    feat: 测试反射代码

--- src/main/java/com/dukz/jvm/ClassTest.java
+++ src/main/java/com/dukz/jvm/ClassTest.java
@@ -1,5 +1,7 @@
 package com.dukz.jvm;
 
+import java.lang.reflect.Field;
+
 /**
  * @ClassName ClassTest
  * @Description ClassTest
@@ -8,4 +10,41 @@ package com.dukz.jvm;
  * @Version 1.0
  */
 public class ClassTest {
+    private static int[] array = new int[3];
+    private static int length = array.length;
+
+    private static Class<One> one = One.class;
+    private static Class<Another> another = Another.class;
+
+    public static void main(String[] args) throws Exception {
+        One oneObject = one.newInstance();
+        oneObject.call();
+
+        Another anotherObject = another.newInstance();
+        anotherObject.speak();
+
+        Field privateFieldInOne = one.getDeclaredField("inner");
+        privateFieldInOne.setAccessible(true);
+        privateFieldInOne.set(oneObject, "world changed.");
+
+        System.out.println(oneObject.getInner());
+    }
+}
+
+class One {
+    private String inner = "time flies.";
+
+    public void call (){
+        System.out.println("hello world.");
+    }
+
+    public String getInner(){
+        return inner;
+    }
+}
+
+class Another{
+    public void speak(){
+        System.out.println("easy coding.");
+    }
 }
