8b8175351a5fad09f1717c65483361d8f05b696b
dukz <dkz359@163.com>
2021-01-28 00:38:07 +0800

    feat: 添加日志功能
    集成slf4j+logback实现日志功能。

--- src/main/java/com/dukz/test/TestTemp.java
+++ src/main/java/com/dukz/test/TestTemp.java
@@ -1,5 +1,8 @@
 package com.dukz.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 
 /**
@@ -10,6 +13,8 @@ import java.util.HashMap;
  * @Version 1.0
  */
 public class TestTemp {
+    public static final Logger logger = LoggerFactory.getLogger(TestTemp.class);
+
     public static void main(String[] args) {
         Integer i1 = new Integer(123);
         Integer i2 = new Integer(123);
@@ -22,5 +27,12 @@ public class TestTemp {
         Integer i5 = Integer.valueOf(123);
         Integer i6 = Integer.valueOf(123);
         System.out.println(i5 == i6);
+
+        int a =10,b=0;
+        try {
+            int c = a/b;
+        } catch (Exception e) {
+            logger.error("除法出错了，"+e.getMessage(), e);
+        }
     }
 }
