7eebe8fa4b249ed62a173526951854ac27e8a336
dukezheng <dukezheng@thunisoft.com>
2021-03-23 12:28:41 +0800

    feat：线程池测试代码

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author dukezheng
@@ -10,7 +12,13 @@ import java.util.Map;
  */
 public class Test {
 
+    public static final Pattern pattern = Pattern.compile("(\\d)+");
     public static void main(String[] args) {
-        System.out.println(Thread.currentThread().getName());
+        String ah = "（2021）湘民初1000号";
+        Matcher matcher = pattern.matcher(ah);
+        matcher.find();
+        System.out.println(matcher.group());
+        matcher.find();
+        System.out.println(matcher.group());
     }
 }
