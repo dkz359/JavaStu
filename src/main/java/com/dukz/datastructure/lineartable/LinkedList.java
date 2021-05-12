5b1f9b371642983078c18d5d40db3b10cb4038e9
dukezheng <dukezheng@thunisoft.com>
2021-05-12 20:52:48 +0800

    feat：添加数据结构

---
+++ src/main/java/com/dukz/datastructure/lineartable/LinkedList.java
@@ -0,0 +1,22 @@
+package com.dukz.datastructure.lineartable;
+
+/**
+ * @author dukezheng
+ * @ClassName LinkedList
+ * @Description 链表数据结构
+ * @date 2021/5/12 20:50
+ * @Version 1.0
+ */
+public class LinkedList {
+
+}
+
+/**
+ * @Author dukezheng
+ * @Description 链表的节点
+ * @Date 20:50 2021/5/12
+ */
+class Node<T>{
+	private Node next;
+	private T data;
+}
