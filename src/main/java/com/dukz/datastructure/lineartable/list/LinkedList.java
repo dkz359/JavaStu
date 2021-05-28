a7796429b5c4d1726562f055fa9ced46412aeb5c
dukezheng <dukezheng@thunisoft.com>
2021-05-28 17:53:27 +0800

    feat：添加单向链表逻辑

--- src/main/java/com/dukz/datastructure/lineartable/list/LinkedList.java
+++ src/main/java/com/dukz/datastructure/lineartable/list/LinkedList.java
@@ -1,5 +1,7 @@
 package com.dukz.datastructure.lineartable.list;
 
+import org.w3c.dom.Node;
+
 /**
  * @author dukezheng
  * @ClassName LinkedList
@@ -7,7 +9,7 @@ package com.dukz.datastructure.lineartable.list;
  * @date 2021/5/12 20:50
  * @Version 1.0
  */
-public class LinkedList {
+public class LinkedList<T> {
 	/**
 	 * @Author dukezheng
 	 * @Description 单向链表的节点
@@ -23,6 +25,65 @@ public class LinkedList {
 		}
 	}
 
-	private Node head;
+	public LinkedList(Node head) {
+		this.head = head;
+	}
+
+	public LinkedList() {
+	}
+
+	/**
+	 * 头结点，初始化空的结点
+	 */
+	private Node head = new Node(null, null);
+
+	/**
+	 * 链表添加元素，头插法
+	 * @param data
+	 * @param <T>
+	 * @return
+	 */
+	public <T> boolean add(T data){
+		Node<T> node = new Node<>(head.next, data);
+		head.next = node;
+		return true;
+	}
+
+	/**
+	 * 删除指定值的元素
+	 * @param o
+	 * @return
+	 */
+	public boolean remove(Object o){
+		if(o == null){
+			for (Node<T> x=head.next,pre = head; x!=null; pre=x,x=x.next){
+				if(x.data == null){
+					pre.next = x.next;
+					x=null;
+					return true;
+				}
+			}
+		}else{
+			for (Node<T> x=head.next,pre = head; x!=null; pre=x,x=x.next){
+				if(o.equals(x.data)){
+					pre.next = x.next;
+					x=null;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 打印list
+	 */
+	public <T> void printList(){
+		Node<T> cur = head;
+		while (cur.next!=null){
+			cur = cur.next;
+			System.out.print(cur.data+" ");
+		}
+	}
 }
 
---
+++ src/main/java/com/dukz/datastructure/lineartable/stack/LinkedStack.java
@@ -0,0 +1,11 @@
+package com.dukz.datastructure.lineartable.stack;
+
+/**
+ * @author dukezheng
+ * @ClassName LinkedStack
+ * @Description 链式栈
+ * @date 2021/5/25 20:50
+ * @Version 1.0
+ */
+public class LinkedStack {
+}
