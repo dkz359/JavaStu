c9f89b9a184538f0c52da17de8e1b10b72273792
dukezheng <dukezheng@thunisoft.com>
2021-08-11 12:46:16 +0800

    feat：二分查找进阶

--- src/main/java/com/dukz/algorithm/search/BinarySearch.java
+++ src/main/java/com/dukz/algorithm/search/BinarySearch.java
@@ -14,7 +14,7 @@ public class BinarySearch {
 
 	private static int binarySearch(int[] num, int low, int high, int val){
 		if(low > high){ return -1;}
-		int mid = low + ((high-low)>>2);
+		int mid = low + ((high-low)>>1);
 		if(num[mid] == val){
 			return mid;
 		}
@@ -23,5 +23,111 @@ public class BinarySearch {
 		}else{
 			return binarySearch(num, low, mid-1, val);
 		}
+	}
+
+
+	/**
+	 * @Author dukezheng
+	 * @Description 查找第一个等于给定值
+	 * @Date 21:15 2021/8/10
+	 * @Param 
+	 * @return 
+	 */
+	public static int firstEqualSearch(int[] num, int n, int val){
+		int low = 0,high = n-1;
+		int mid;
+		while (low <= high){
+			mid = low + ((high - low)>>1);
+			if(num[mid] < val){
+				low = mid +1;
+			}else if(num[mid] > val){
+				high = mid -1;
+			}else{
+				if(mid == 0 || num[mid-1] != val){
+					return mid;
+				}else{
+					high = mid - 1;
+				}
+			}
+		}
+		return -1;
+	}
+
+	/**
+	 * @Author dukezheng
+	 * @Description 查找最后一个等于给定值
+	 * @Date 10:14 2021/8/11
+	 * @Param
+	 * @return
+	 */
+	public static int lastEqualSearch(int[] num, int n, int val){
+		int low = 0,high = n-1;
+		int mid;
+		while (low <= high){
+			mid = low + ((high - low)>>1);
+			if(num[mid] < val){
+				low = mid +1;
+			}else if(num[mid] > val){
+				high = mid -1;
+			}else{
+				if(mid == n-1 || num[mid+1] != val){
+					return mid;
+				}else{
+					low = mid + 1;
+				}
+			}
+		}
+		return -1;
+	}
+
+	/**
+	 * @Author dukezheng
+	 * @Description 第一个大于等于给定值
+	 * @Date 12:38 2021/8/11
+	 * @Param
+	 * @return
+	 */
+	public static int firstGreaterEqualSearch(int[] num, int n, int val){
+		int low = 0,high = n-1;
+		int mid;
+		while (low <= high){
+			mid = low + ((high - low)>>1);
+			if(num[mid] < val){
+				low = mid +1;
+			}else{
+				if(mid == 0 || num[mid-1] < val){
+					return mid;
+				}else{
+					high = mid - 1;
+				}
+			}
+		}
+		return -1;
+	}
+
+
+	/**
+	 * @Author dukezheng
+	 * @Description 查找最后一个小于等于给定值
+	 * @Date 10:14 2021/8/11
+	 * @Param
+	 * @return
+	 */
+	public static int lastLessEqualSearch(int[] num, int n, int val){
+		int low = 0,high = n-1;
+		int mid;
+		while (low <= high){
+			mid = low + ((high - low)>>1);
+			if(num[mid] > val){
+				high = mid -1;
+			}else{
+				if(mid == n-1 || num[mid+1] > val){
+					return mid;
+				}else{
+					low = mid + 1;
+				}
+			}
+		}
+		return -1;
 	}
 }
--- src/main/java/com/dukz/algorithm/search/TestSearch.java
+++ src/main/java/com/dukz/algorithm/search/TestSearch.java
@@ -12,5 +12,15 @@ public class TestSearch {
 		int[] num = new int[]{1,2,3,5,6,7,9,11,13};
 		int index = BinarySearch.search(num, num.length, 7);
 		System.out.println(index);
+
+		int[] num1 = new int[]{1,2,3,7,7,7,9,11,13};
+		int index1 = BinarySearch.firstEqualSearch(num1, num1.length, 7);
+		System.out.println(index1);
+		int index2 = BinarySearch.lastEqualSearch(num1, num1.length, 7);
+		System.out.println(index2);
+		int index3 = BinarySearch.firstGreaterEqualSearch(num1, num1.length, 8);
+		System.out.println(index3);
+		int index4 = BinarySearch.lastLessEqualSearch(num1, num1.length, 8);
+		System.out.println(index4);
 	}
 }
